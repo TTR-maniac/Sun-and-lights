7bee4c6688909049895855be14b6da9c0d8c4101
Adam <76447994+TTR-maniac@users.noreply.github.com>
2021-02-28 12:33:29 -0500

    Create sun.py

---
+++ sun.py
@@ -0,0 +1,24 @@
+import turtle
+
+forw = 4
+
+turt = turtle. Turtle()
+turt.speed(100)
+
+while True:
+    turt.color('yellow')
+    turt.forward(forw)
+    turt.left(420)
+    turt.left(133)
+    forw += 3
+    turt.right(320)
+    forw += 3
+    turt.right(82)
+    forw += 1
+    turt.pensize(9)
+    turt.left(50)
+    turt.left(1)
+    turt.right(7)
+    from turtle import * 
+    bgcolor("cyan")
+turtle.done()
