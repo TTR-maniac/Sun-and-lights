ecb7fe592d11101669bb410819a192ccc389a0a9
Adam <76447994+TTR-maniac@users.noreply.github.com>
2021-02-28 12:48:24 -0500

    Create weird draw.py

---
+++ Fun projects/weird draw.py
@@ -0,0 +1,21 @@
+import turtle
+
+forw = 3
+
+turt = turtle. Turtle()
+turt.speed(0)
+
+while True:
+    turt.forward(forw)
+    turt.left(109)
+    turt.left(5)
+    forw += 7
+    turt.left(21)
+    forw += 3
+    turt.left(30)
+    turt.write(';LIGHT')
+    turt.dot(20)
+    turt._pen
+    turt.pensize(8)
+
+turtle.done()
